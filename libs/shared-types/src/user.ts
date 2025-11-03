dd9dcadc38b279336d34342e5753298f760d64fe
d-caruso <caruso.domenico@gmail.com>
2025-11-03 22:26:54 +0100

    [TASK] 1.1 Add Admin Role to User Model

--- libs/shared-types/src/user.ts
+++ libs/shared-types/src/user.ts
@@ -2,12 +2,15 @@
  * User-related type definitions
  */
 
+export type UserRole = 'user' | 'admin';
+
 export interface User {
   id: number;
   email: string;
   name: string;
   surname: string;
   isActive: boolean;
+  role: UserRole;
   creationDate: string;
   updateDate: string;
 }
