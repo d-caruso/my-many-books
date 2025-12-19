7560117a4652145d17a1f5331ab87fa644ddaad9
d-caruso <caruso.domenico@gmail.com>
2025-12-19 11:05:43 +0100

    [TEST] 1.1 create Jest config and TypeScript test configuration

---
+++ libs/shared-api/jest.config.ts
@@ -0,0 +1,25 @@
+export default {
+  displayName: 'shared-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/shared-api',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts',
+    '!src/**/__mocks__/**',
+    '!src/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
