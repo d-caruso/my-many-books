5a3832900d028b020842811837c5c4fcec1b6fbb
d-caruso <caruso.domenico@gmail.com>
2025-12-19 17:05:06 +0100

    [TEST] 7.1 setup Jest config for shared-utils

---
+++ libs/shared-utils/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'shared-utils',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/shared-utils',
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/**/*.test.ts', '!src/index.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
---
+++ libs/shared-utils/src/__tests__/smoke.test.ts
@@ -0,0 +1,8 @@
+import * as utils from '../index';
+
+describe('shared-utils exports', () => {
+  test('exports utility functions', () => {
+    expect(utils).toBeDefined();
+  });
+});
+
