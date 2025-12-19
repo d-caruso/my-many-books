506036ae448008101aadf33c2efe6ae57771adeb
d-caruso <caruso.domenico@gmail.com>
2025-12-19 18:39:57 +0100

    [TEST] 12.1 setup @testing-library/react-hooks

--- libs/shared-ui-hooks/jest.config.ts
+++ libs/shared-ui-hooks/jest.config.ts
@@ -1,17 +1,33 @@
-/* eslint-disable */
-module.exports = {
+export default {
   displayName: 'shared-ui-hooks',
   preset: '../../jest.preset.js',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
     '^.+\\.[tj]sx?$': [
       'ts-jest',
       {
-        tsconfig: '<rootDir>/tsconfig.lib.json',
+        tsconfig: '<rootDir>/tsconfig.spec.json',
       },
     ],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/shared-ui-hooks',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.spec.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/__tests__/**/*',
+    '!src/index.ts',
+    '!src/setupTests.ts',
+    '!src/examples/**/*',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
