278be9fda1a80b937aacee1a7ba3dd7da6fad0ca
d-caruso <caruso.domenico@gmail.com>
2025-12-20 01:22:57 +0100

    [TEST] 5.2 fix shared-i18n fallbacks test lint

--- libs/shared-i18n/src/__tests__/fallbacks.test.ts
+++ libs/shared-i18n/src/__tests__/fallbacks.test.ts
@@ -1,19 +1,18 @@
 describe('shared-i18n fallbacks', () => {
   test('initializeI18n uses DEFAULT_LANGUAGE when language is not provided', async () => {
     jest.resetModules();
-    const { DEFAULT_LANGUAGE } = require('../types');
-    const { initializeI18n, getCurrentLanguage } = require('../config');
+    const { DEFAULT_LANGUAGE } = await import('../types');
+    const { initializeI18n, getCurrentLanguage } = await import('../config');
 
     await initializeI18n();
     expect(getCurrentLanguage()).toBe(DEFAULT_LANGUAGE);
   });
 
-  test('getCurrentLanguage returns DEFAULT_LANGUAGE before initialization', () => {
+  test('getCurrentLanguage returns DEFAULT_LANGUAGE before initialization', async () => {
     jest.resetModules();
-    const { DEFAULT_LANGUAGE } = require('../types');
-    const { getCurrentLanguage } = require('../config');
+    const { DEFAULT_LANGUAGE } = await import('../types');
+    const { getCurrentLanguage } = await import('../config');
 
     expect(getCurrentLanguage()).toBe(DEFAULT_LANGUAGE);
   });
 });
-
