dd7b073b2de885e5cb7b546db2e6e5a5cc158c9e
d-caruso <caruso.domenico@gmail.com>
2025-12-27 02:53:16 +0100

    [TASK] 1.2.4 export search translations from shared-i18n config

--- libs/shared-i18n/src/config.ts
+++ libs/shared-i18n/src/config.ts
@@ -9,6 +9,7 @@ import enBooks from './locales/en/books.json';
 import enAdmin from './locales/en/pages.json';
 import enHooks from './locales/en/hooks.json';
 import enDialogs from './locales/en/dialogs.json';
+import enSearch from './locales/en/search.json';
 
 import itCommon from './locales/it/common.json';
 import itValidation from './locales/it/validation.json';
@@ -17,6 +18,7 @@ import itBooks from './locales/it/books.json';
 import itAdmin from './locales/it/pages.json';
 import itHooks from './locales/it/hooks.json';
 import itDialogs from './locales/it/dialogs.json';
+import itSearch from './locales/it/search.json';
 
 /**
  * Initialize i18next with all translations
@@ -37,6 +39,7 @@ export const initializeI18n = async (language?: string) => {
       TranslationNamespace.ADMIN,
       TranslationNamespace.HOOKS,
       TranslationNamespace.DIALOGS,
+      TranslationNamespace.SEARCH,
     ],
     defaultNS: TranslationNamespace.COMMON,
 
@@ -50,6 +53,7 @@ export const initializeI18n = async (language?: string) => {
         [TranslationNamespace.ADMIN]: enAdmin,
         [TranslationNamespace.HOOKS]: enHooks,
         [TranslationNamespace.DIALOGS]: enDialogs,
+        [TranslationNamespace.SEARCH]: enSearch,
       },
       it: {
         [TranslationNamespace.COMMON]: itCommon,
@@ -59,6 +63,7 @@ export const initializeI18n = async (language?: string) => {
         [TranslationNamespace.ADMIN]: itAdmin,
         [TranslationNamespace.HOOKS]: itHooks,
         [TranslationNamespace.DIALOGS]: itDialogs,
+        [TranslationNamespace.SEARCH]: itSearch,
       },
     },
 
